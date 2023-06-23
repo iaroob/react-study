9939e0c278523ed431ecb02f09a5b4a6d24ea650
Aroob <aroob330@gmail.com>
2023-06-23 10:54:41 +0200

    fetch examples

---
+++ week-2/fetch/fetch-example.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from 'react';
+
+export default function App() {
+  const [data, setData] = useState({});
+
+  const fetchData = () => { 
+    fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`) 
+      .then((response) => response.json()) 
+      .then((jsonData) => setBtcData(jsonData.bpi.USD)) 
+      .catch((error) => console.log(error)); 
+  };
+  useEffect(() => { 
+    fetchData();
+  }, []);
+
+  return (
+    <div> 
+      <h1>Current BTC/USD data</h1> 
+      <p>Code: {btcData.code}</p> 
+      <p>Symbol: {btcData.symbol}</p> 
+      <p>Rate: {btcData.rate}</p> 
+      <p>Description: {btcData.description}</p> 
+      <p>Rate Float: {btcData.rate_float}</p> 
+    </div>
+  );
+}
